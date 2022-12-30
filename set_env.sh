5b1fba49319249f335284d9fa4bc86e8cd5eef69
Ganesh Kasi <ganeshkasi@hotmail.com>
2022-12-30 17:40:47 +0800

    Config

---
+++ set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=ganeshkasi
+export POSTGRES_PASSWORD=ganeshkasi
+export POSTGRES_HOST=ganeshkasi-rds.cfywob1wrqgf.us-east-2.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=ganeshkasi-dev
+export AWS_REGION=us-east-2
+export AWS_PROFILE=default
+export JWT_SECRET=hello
+export URL=http://localhost:8100
